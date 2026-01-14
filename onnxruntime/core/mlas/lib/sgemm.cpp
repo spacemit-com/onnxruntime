64adc986787eaeadfe8b83100a7ea5cc2416c847
Colm Donelan <52702205+Colm-in-Arm@users.noreply.github.com>
2026-01-14 00:57:49 +0000

    Allow MSVC to build KleidiAI in Windows on Arm environments. (#26995)
    ### Description
    Remove the limitations on using onnxruntime_USE_KLEIDIAI in a Windows on
    Arm environment.
    
    ### Motivation and Context
    Historically the KleidiAI build had difficulties with using Microsoft
    compiler for Arm environments (MSVC). As a result a hard exclusion of
    onnxruntime_USE_KLEIDIAI and MSVC was added and subsequently
    consolidated into cmake/CMakeLists.txt by
    [this](https://github.com/microsoft/onnxruntime/commit/2e8a45ab059e3bff81a663d15a2b10e8c9e639ef)
    commit.
    
    The problems in KleidiAI were resolved in their v1.14.0 release. v1.15.0
    was introduced via
    [this](https://github.com/microsoft/onnxruntime/commit/8fe48049c089ec50f6f915ca97c37969b7194a96)
    commit. This PR removes the limitation, allowing MSVC to be used to
    compile with onnxruntime_USE_KLEIDIAI enabled in a Winodws on Arm
    environment.
    
    In addition there were legacy restrictions in CMakeLists.txt relating to
    DOTPROD and I8MM CPU features. This is already handled in the KleidiAI
    build.
    ### Verification
    Following the Windows build instructions
    [here](https://onnxruntime.ai/docs/build/inferencing.html#windows)
    KleidiAI and its associated logic in MLAS will be built when ARM64 is
    detected.
    
    **Note**: As is made clear in these build instructions MSVC must include
    support for ARM64. Both Python and Cmake must be native ARM64.
    
    Signed-off-by: Colm Donelan <colm.donelan@arm.com>

--- onnxruntime/core/mlas/lib/sgemm.cpp
+++ onnxruntime/core/mlas/lib/sgemm.cpp
@@ -1686,7 +1686,7 @@ Return Value:
     // Compute the number of bytes required to hold the packed buffer.
     //
     // KleidiAI or other override
-    #if defined(USE_KLEIDIAI) && !defined(_MSC_VER)
+    #if defined(USE_KLEIDIAI)
     if (GetMlasPlatform().MlasGemmPackBSizeOverride != nullptr &&
         // TODO: Remove once KAI supports transposing for A
         TransA != CBLAS_TRANSPOSE::CblasTrans) {
@@ -1753,7 +1753,7 @@ Return Value:
 
 --*/
 {
-#if defined(USE_KLEIDIAI) && !defined(_MSC_VER)
+#if defined(USE_KLEIDIAI)
     if (GetMlasPlatform().MlasGemmPackBOverride != nullptr  &&
         // TODO: Remove once KAI supports transposing for A
         TransA != CBLAS_TRANSPOSE::CblasTrans    &&
